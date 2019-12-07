ee234659897b889e025348a5eccd15c9b4296110
Joel Säll <joel.sael@gmail.com>
2019-12-07 13:04:54 +0100

    Day1 task 2 solved.

--- src/day1/fuel.cpp
+++ src/day1/fuel.cpp
@@ -2,15 +2,29 @@
 //#include <iostream>
 #include <numeric>
 
+namespace
+{
+    auto formula(int a, int b) -> int
+    {
+        // std::cout << "a: " << a << " b: " << b << "\n";
+        int ret = 0;
+        constexpr int minCalcuableWeight = 8; // 8 / 3 - 2 = 0, smallest number we allow to calculate with.
+        if (b < minCalcuableWeight) {
+            ret = a;
+        }
+        else {
+            auto tmp = b / 3 - 2;
+            // return a + (tmp >= 2 ? tmp - 2 : tmp);
+            ret = formula(a + tmp, tmp);
+        }
+        return ret;
+    }
+} // namespace
+
 namespace Fuel
 {
     int accumulator(const std::vector<int>& weights)
     {
-        auto formula = [](int a, int b) {
-            // std::cout << "a: " << a << " b: " << b << "\n";
-            auto tmp = b / 3;
-            return a + (tmp >= 2 ? tmp - 2 : tmp);
-        };
 
         auto sum = std::accumulate(weights.begin(), weights.end(), 0, formula);
         // std::cout << "woooza: " << sum << "\n";
--- test/day1/day1.cpp
+++ test/day1/day1.cpp
@@ -1,5 +1,3 @@
-
-//#include "day1.hpp"
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch2/catch.hpp"
 #include "fuel.hpp"
@@ -7,30 +5,14 @@
 
 TEST_CASE("Fuel accumulator", "[woot]")
 {
-    // std::vector<int> tst {9, 9};
-    SECTION("Test with large weights")
+    SECTION("Test with small weights")
     {
         REQUIRE(Fuel::accumulator({ 9, 9 }) == 2);
         REQUIRE(Fuel::accumulator({ 9, 9, 9 }) == 3);
-        REQUIRE(Fuel::accumulator({ 99, 99 }) == 62);
     }
 
-    // Can we go below zero or stop at zero?
-
-    SECTION("Test with odd weights")
+    SECTION("Test with Advents example weights")
     {
-        REQUIRE(Fuel::accumulator({ 11, 12 }) == 3);
-        REQUIRE(Fuel::accumulator({ 13, 13, 13 }) == 6);
-        REQUIRE(Fuel::accumulator({ 22, 22 }) == 10);
-    }
-
-    SECTION("Test with small weights")
-    {
-        REQUIRE(Fuel::accumulator({ 3, 3 }) == 2);
-    }
-
-    SECTION("Test with Advents examplessmall weights")
-    {
-        REQUIRE(Fuel::accumulator({ 12, 14, 1969, 100756 }) == 34241);
+        REQUIRE(Fuel::accumulator({ 14, 1969, 100756 }) == 51314);
     }
 }
